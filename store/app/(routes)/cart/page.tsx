85e3ed7f2794da78c4f9ad6422fd2ce37f2e488d
Rahul Kumar <rahul@aerosimple.in>
2023-09-11 17:02:01 +0530

    store position 0

--- store/app/(routes)/cart/page.tsx
+++ store/app/(routes)/cart/page.tsx
@@ -24,9 +24,9 @@ const CartPage = () => {
                     <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
                     <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
                         <div className="lg:col-span-7">
-                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart</p>}
+                            {cart?.items?.length === 0 && <p className="text-neutral-500">No items added to cart</p>}
                             <ul>
-                                {cart.items.map(item => (
+                                {cart?.items?.map(item => (
                                     <CartItem
                                         key={item.id}
                                         data={item}
