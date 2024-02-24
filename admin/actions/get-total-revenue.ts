7e247e032697b3ae722ecadc7b0f74653ab59b82
Rahul Kumar <rahul@aerosimple.in>
2024-02-24 16:09:06 +0530

    change  nUmber

--- admin/actions/get-total-revenue.ts
+++ admin/actions/get-total-revenue.ts
@@ -17,7 +17,7 @@ export const getTotalRevenue = async (storeId: string) => {
 
     const totalRevenue = paidOrders.reduce((total, order) => {
         const orderTotal = order.orderItems.reduce((orderSum, item) => {
-            return orderSum + item.product.price.toNumber();
+            return orderSum + Number(item.product.price);
         }, 0);
         return total + orderTotal;
     }, 0);
