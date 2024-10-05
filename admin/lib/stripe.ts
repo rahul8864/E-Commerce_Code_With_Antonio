f33fc563761c9c780f23116f57e56e70bb9117d7
Rahul Kumar <rahulkumar8864084963@gmail.com>
2024-10-05 23:13:27 +0530

    update stripe version

--- admin/lib/stripe.ts
+++ admin/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe  = new Stripe(process.env.STRIPE_API_KEY!, {
-    apiVersion: "2024-04-10",
+    apiVersion: "2024-09-30.acacia",
     typescript: true,
 });
