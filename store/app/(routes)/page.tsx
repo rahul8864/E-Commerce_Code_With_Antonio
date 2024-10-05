a083164abbef2c1f7538b0e70c55b466dfef0898
Rahul Kumar <rahulkumar8864084963@gmail.com>
2024-10-06 00:17:55 +0530

    fix issue

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -7,7 +7,7 @@ import ProductList from "@/components/product-list";
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const billboard = await getBillboard('df0e09f1-ec53-4f56-96b8-6414c467f7af');
+    const billboard = await getBillboard('1ed54d58-f16f-420e-8750-9c9cccdeedb3');
     const products = await getProducts({ isFeatured: true })
     return (
         <Container>
